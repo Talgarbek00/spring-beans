729a46d172305d49fc5c95ea6c6ec9a602ee3c61
talgar <talgarbeknazarbekov00@gmail.com>
2022-02-11 22:04:38 +0600

    commit

--- src/main/java/peaksoft/model/AnimalsCage.java
+++ src/main/java/peaksoft/model/AnimalsCage.java
@@ -1,13 +1,32 @@
 package peaksoft.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
     private Animal animal;
+    private Timer timer;
+
+    public Animal getAnimal() {
+        return animal;
+    }
+    @Autowired
+    @Qualifier("t")
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+    @Autowired
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
---
+++ src/main/java/peaksoft/model/Dog.java
@@ -0,0 +1,13 @@
+package peaksoft.model;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("t")
+public class Dog extends Animal{
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
+}
